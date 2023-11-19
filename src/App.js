d3695cdbefdde014010da4b73675775b63690e8d
Karly Stark <karlystark@gmail.com>
2023-11-18 17:04:11 -0800

    WIP building out filtering component

--- src/App.js
+++ src/App.js
@@ -30,8 +30,32 @@ function App() {
     description: "I made a cake! please eat some! it's spice cake with mascarpone whipped cream frosting and raspberry jam.",
     image: "cake.jpeg",
     quantity: 3
+  },
+  {
+    id: 4,
+    user: "karlystark",
+    title: "pet care",
+    description: "I can feed your cat or hang with your dog any time you're away! my job is basically cats!",
+    image: "darla.jpeg",
+    quantity: 1
+  },
+  {
+    id: 5,
+    user: "karlystark",
+    title: "REI Passage 2 tent",
+    description: "A two-person REI Passage 2 tent.",
+    image: "passage2.jpeg",
+    quantity: 1
+  },
+  {
+    id: 6,
+    user: "karlystark",
+    title: "wooden spoons",
+    description: "I whittle so many of these, have some!",
+    image: "spoons.jpeg",
+    quantity: 10
+  },
 
-  }
  ]
 
 
--- src/ResourceList.js
+++ src/ResourceList.js
@@ -1,8 +1,35 @@
+import React, { useState } from 'react';
 import ResourceCard from "./ResourceCard.js";
 import "./ResourceList.css";
 
 
 function ResourceList({ resources }) {
+  const [isClicked, setIsClicked] = useState(null);
+
+  console.log("isClicked", isClicked);
+
+  const tags = [
+    "tools",
+    "foods",
+    "services",
+    "auto/bike",
+    "outdoors"
+  ];
+
+  const friends = [
+    "whitney",
+    "jen",
+    "george",
+    "bug"
+  ]
+
+  function handleClick(idx) {
+    setIsClicked(idx);
+  }
+
+  function resetFilters(){
+    setIsClicked(null);
+  }
 
   return (
     <div className="ResourceList">
@@ -11,11 +38,39 @@ function ResourceList({ resources }) {
         <h1 className="ResourceList-banner-title"> available resources </h1>
         <img src="carrots.png" alt="" />
       </div>
-      <div className="ResourceList-list">
-        {resources.map(resource =>
-          <ResourceCard resource={resource} />)}
+      <div className="ResourceList-body">
+
+        <div className="ResourceList-filter">
+          <p className="ResourceList-filter-head">filter</p>
+          <div className="ResourceList-filter-category">
+            <p className="ResourceList-filter-title">TYPE</p>
+            {tags.map((el, idx) =>
+              <div className="ResourceList-filter-tag">
+                <button type="button" onClick={() => handleClick(idx)}>
+                  <i className={isClicked === idx ? "bi bi-square-fill" : "bi bi-square"} />
+                </button>
+                <p>{el}</p>
+              </div>)}
+          </div>
+          <div className="ResourceList-filter-friends">
+            <p className="ResourceList-filter-title">FRIENDS</p>
+            {friends.map((el, idx) =>
+              <div className="ResourceList-filter-tag">
+                <button type="button" onClick={() => handleClick(idx)}>
+                  <i className={isClicked === idx ? "bi bi-square-fill" : "bi bi-square"} />
+                </button>
+                <p>{el}</p>
+              </div>)}
+          </div>
+          <button type="button" className="btn btn-outline-dark ResourceList-filter-reset" onClick={() => resetFilters()}>reset filters</button>
+        </div>
+
+        <div className="ResourceList-list">
+          {resources.map(resource =>
+            <ResourceCard resource={resource} />)}
+        </div>
       </div>
-    </div >
+    </div>
   );
 }
 
