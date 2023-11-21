a576a10969842378bd18d9924a3131d1c25fbe30
Karly Stark <karlystark@gmail.com>
2023-11-21 12:14:43 -0800

    edited homepage and made signup route

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,20 @@
+import UserCard from "./UserCard.js";
+import "./Home.css";
 
+function Home({ resources, isLoggedIn }) {
 
-function Home(){
-
+  return (
+    <div className="Home">
+      {isLoggedIn?
+      <h1>Welcome back {resources.user}!</h1>
+      :
+      <div className="Home-new">
+      <h1>Welcome to the Shed!</h1>
+      <p>Login or create an account to get started.</p>
+      </div>}
+      {/* <UserCard resources={resources} /> */}
+    </div>
+  );
 }
 
 export default Home;
--- src/RoutesList.js
+++ src/RoutesList.js
@@ -6,17 +6,19 @@ import ResourceList from "./ResourceList.js";
 import ResourceDetail from "./ResourceDetail.js";
 import UserProfile from "./UserProfile.js";
 import NewResourceForm from "./NewResourceForm.js";
+import SignupForm from "./SignupForm.js";
 
-function RoutesList({resources, handleAddResource}) {
+function RoutesList({resources, handleAddResource, handleAddUser, isLoggedIn}) {
 
   return (
     <Routes>
-      <Route path="/" element={<Home resources={resources}/>} />
+      <Route path="/" element={<Home resources={resources} isLoggedIn={isLoggedIn}/>} />
       <Route path="/about" element={<About />} />
       <Route path="/users/karlystark" element={<UserProfile resources={resources} />} />
       <Route path="/resources" element={<ResourceList resources={resources}/>} />
       <Route path="/resources/:id" element={<ResourceDetail resources={resources}/>} />
       <Route path="/resources/upload" element={<NewResourceForm resources={resources} handleAddResource={handleAddResource}/>} />
+      <Route path="/signup" element={<SignupForm handleAddUser={handleAddUser}/>} />
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   );
